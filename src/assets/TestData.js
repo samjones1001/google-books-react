0941152b2867940d806d80e35fffb943043d883c
Sam Jones <sam.jones494@googlemail.com>
2019-06-15 16:16:53 +0100

    adds test data

---
+++ src/assets/TestData.js
@@ -0,0 +1,37 @@
+"items" : [
+  {
+    "title": "The C++ Programming Language",
+    "authors": ["Bjarne Stroustrup"],
+    "publisher": "Addison-Wesley",
+    "imageLink": "http://books.google.com/books/content?id=PSUNAAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api" ,
+    "infoLink": "https://play.google.com/store/books/details?id=PSUNAAAAQBAJ&source=gbs_api"
+  },
+  {
+    "title": "COMPUTER PROGRAMMING IN C",
+    "authors": ["V. RAJARAMAN"],
+    "publisher": "PHI Learning Pvt. Ltd.",
+    "imageLink": "http://books.google.com/books/content?id=Rl-F95_f0GoC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api" ,
+    "infoLink": "https://play.google.com/store/books/details?id=Rl-F95_f0GoC&source=gbs_api"
+  },
+  {
+    "title": "Programming In C",
+    "authors": ["D Ravichandran"],
+    "publisher": "New Age International",
+    "imageLink": "http://books.google.com/books/content?id=CEc2WsZkxUsC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api" ,
+    "infoLink": "http://books.google.co.uk/books?id=CEc2WsZkxUsC&dq=programming&hl=&source=gbs_api"
+  },
+  {
+    "title": "Python Programming",
+    "authors": ["John M. Zelle"],
+    "publisher": "Franklin, Beedle & Associates, Inc.",
+    "imageLink": "http://books.google.com/books/content?id=aJQILlLxRmAC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api" ,
+    "infoLink": "http://books.google.co.uk/books?id=aJQILlLxRmAC&dq=programming&hl=&source=gbs_api"
+  },
+  {
+    "title": "Programming Pearls",
+    "authors": ["Jon Bentley"],
+    "publisher": "Addison-Wesley Professional",
+    "imageLink": "http://books.google.com/books/content?id=4gX0CwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api" ,
+    "infoLink": "https://play.google.com/store/books/details?id=4gX0CwAAQBAJ&source=gbs_api"
+  }
+]
