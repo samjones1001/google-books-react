29fcb4e007287b82ad46586b8112d2133542e98c
Sam Jones <sam.jones494@googlemail.com>
2019-06-15 22:20:30 +0100

    refactors Book component

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,31 +1,22 @@
 import React from 'react';
 
 const Book = (props) => {
-  const { title, authors, infolink } = props
-  const imagelink = props.imagelink !== undefined
-    ? props.imagelink
-    : require('../../assets/book-placeholder.png');
-
-  const publisher = props.publisher !== undefined
-    ? props.publisher
-    : 'No publisher found';
-
-  const buildAuthorString = () => {
-    return authors !== undefined
-      ? authors.join(', ')
-      : 'No author found';
-  }
+  const { title, authors, imagelink, publisher, infolink } = props
+  const imageUrl = imagelink
+    ? imagelink : require('../../assets/book-placeholder.png');
+  const publisherString = publisher ? publisher : 'No publisher found';
+  const authorString = authors ? authors.join(', ') : 'No author found';
 
   return (
     <div data-test="component-book" { ...props }>
       <h3 data-test="book-title">{ title }</h3>
       <img
-        src={ imagelink }
+        src={ imageUrl }
         alt="book cover"
         data-test="book-image"
       />
-      <p data-test="book-author">{ buildAuthorString() }</p>
-      <p data-test="book-publisher">{ publisher }</p>
+      <p data-test="book-author">{ authorString }</p>
+      <p data-test="book-publisher">{ publisherString }</p>
       <a href={ infolink } data-test="book-info-link">More Info</a>
     </div>
   );
