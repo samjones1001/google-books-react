e09cde4bd549d34ad8be6bba059c49fc22e1bafa
Sam Jones <sam.jones494@googlemail.com>
2019-06-15 20:38:46 +0100

    refactors Book component test for reduced repetition

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 const Book = (props) => {
-  const { title, imageLink, author, publisher, infoLink } = props
-
+  const { title, imageLink, authors, publisher, infoLink } = props
   return (
     <div data-test="component-book" { ...props }>
       <h3 data-test="book-title">{ title }</h3>
       <img src={ imageLink } data-test="book-image" />
-      <p data-test="book-author">{ author }</p>
+      <p data-test="book-author">{ authors[0] }</p>
       <p data-test="book-publisher">{ publisher }</p>
       <a href={ infoLink } data-test="book-info-link">More Info</a>
     </div>
--- src/components/Book/Book.test.js
+++ src/components/Book/Book.test.js
@@ -7,43 +7,49 @@ import { testData } from '../../assets/TestData';
 
 Enzyme.configure({ adapter : new EnzymeAdapter() })
 
-it('renders without crashing', () => {
-  const wrapper = shallow(<Book />);
-  const bookComponent = wrapper.find("[data-test='component-book']");
-  expect(bookComponent.exists()).toBe(true);
-});
+describe('Book component', () => {
+  let wrapper;
 
-it('renders the passed title', () => {
-  const title = testData.items[0].title;
-  const wrapper = shallow(<Book title={ title }/>);
-  let titleElement = wrapper.find("[data-test='book-title']");
-  expect(titleElement.text()).toEqual(title);
-});
+  describe('when passed all props', () => {
+    const props = testData.items[0];
 
-it('renders the passed image', () => {
-  const imageLink = testData.items[0].imageLink;
-  const wrapper = shallow(<Book imageLink={ imageLink }/>);
-  let imageElement = wrapper.find("[data-test='book-image']");
-  expect(imageElement.prop('src')).toEqual(imageLink);
-});
+    beforeEach(() => {
+      wrapper = shallow(<Book { ...props }/>);
+    });
 
-it('renders the passed author', () => {
-  const author = testData.items[0].authors[0];
-  const wrapper = shallow(<Book author={ author }/>);
-  let authorElement = wrapper.find("[data-test='book-author']");
-  expect(authorElement.text()).toEqual(author);
-});
+    it('renders without crashing', () => {
+      const bookComponent = wrapper.find("[data-test='component-book']");
+      expect(bookComponent.exists()).toBe(true);
+    });
 
-it('renders the passed publisher', () => {
-  const publisher = testData.items[0].publisher;
-  const wrapper = shallow(<Book publisher={ publisher } />);
-  let publisherElement = wrapper.find("[data-test='book-publisher']");
-  expect(publisherElement.text()).toEqual(publisher);
-});
+    it('renders the passed title', () => {
+      const title = props.title;
+      let titleElement = wrapper.find("[data-test='book-title']");
+      expect(titleElement.text()).toEqual(title);
+    });
 
-it('renders a link to the book\'s info page', () => {
-  const infoLink = testData.items[0].infoLink;
-  const wrapper = shallow(<Book infoLink={ infoLink } />);
-  let infoLinkElement = wrapper.find("[data-test='book-info-link']");
-  expect(infoLinkElement.prop('href')).toEqual(infoLink);
+    it('renders the passed image', () => {
+      const imageLink = props.imageLink;
+      let imageElement = wrapper.find("[data-test='book-image']");
+      expect(imageElement.prop('src')).toEqual(imageLink);
+    });
+
+    it('renders the passed author', () => {
+      const author = props.authors[0];
+      let authorElement = wrapper.find("[data-test='book-author']");
+      expect(authorElement.text()).toEqual(author);
+    });
+
+    it('renders the passed publisher', () => {
+      const publisher = props.publisher;
+      let publisherElement = wrapper.find("[data-test='book-publisher']");
+      expect(publisherElement.text()).toEqual(publisher);
+    });
+
+    it('renders a link to the book\'s info page', () => {
+      const infoLink = testData.items[0].infoLink;
+      let infoLinkElement = wrapper.find("[data-test='book-info-link']");
+      expect(infoLinkElement.prop('href')).toEqual(infoLink);
+    });
+  });
 });
