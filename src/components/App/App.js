05c48f66133fe1ead0b0caec0f11600475159f81
Sam Jones <sam.jones494@googlemail.com>
2019-06-16 09:50:44 +0100

    refactors App to a class component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import BookList from '../BookList/BookList';
+import Search from '../Search/Search';
+
 import { testData } from '../../assets/TestData';
 
-function App() {
-  return (
-    <div className="App" data-test="component-app">
-      <BookList books={testData.items}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App" data-test="component-app">
+        <Search
+          placeholderText="Please search by author or title"
+          buttonText="search"
+        />
+        <BookList books={testData.items}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme';
 
 import App from './App';
 import BookList from '../BookList/BookList';
+import Search from '../Search/Search';
+
 import { findByTestAttr } from '../../utils/testUtils';
 
 describe('App Component', () => {
@@ -15,6 +17,11 @@ describe('App Component', () => {
   it('renders without crashing', () => {
     const appComponent = findByTestAttr(wrapper, 'component-app')
     expect(appComponent.exists()).toBe(true);
+  });
+
+  it('renders a searchComponent', () => {
+    const searchComponent = wrapper.find(Search)
+    expect(searchComponent.exists()).toBe(true);
   });
 
   it('renders a BookList component', () => {
